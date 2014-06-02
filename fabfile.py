4c7f6d0b79045a252f1248cb40689b021ddec4c5
delremm <g1m5yv@gmail.com>
2014-06-02 13:59:00 +0600

    fab prepare_deploy

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,19 @@
-from fabric.api import run
+from fabric.api import run, local, prefix
+
+VIRTUALENV_NAME='food'
 
 def host_type():
     run('uname -s')
+
+def prepare_deploy(test=False):
+    if test:
+        with prefix('workon %s' % VIRTUALENV_NAME):
+            local("./manage.py test")
+    local("git add -u . && git commit")
+    local("git push origin master")
+
+def env_dev():
+    local('export DJANGO_SETTINGS_MODULE=food.settings.dev')
+
+def env_production():
+    local('export DJANGO_SETTINGS_MODULE=food.settings.production')
