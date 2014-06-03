8e49b8918b3a3b60fa7cc79b1a314cc6c12a1896
delremm <g1m5yv@gmail.com>
2014-06-03 12:20:27 +0600

    mysql settings

--- food/settings/dev.py
+++ food/settings/dev.py
@@ -5,3 +5,13 @@ DEBUG = True
 
 MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static/media'))
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static'))
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- food/settings/production.py
+++ food/settings/production.py
@@ -4,3 +4,17 @@ DEBUG = False
 
 MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', '..', 'food_media'))
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', '..', 'food_static'))
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'food',
+        'USER': 'food_admin',
+        'PASSWORD': 'food_admin21',
+        'PORT': '3306',
+        'HOST': '127.0.0.1'
+    }
+}
