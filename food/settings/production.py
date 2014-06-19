d4c2a966f0c22e84440d27584224dfd6fc49146d
delremm <g1m5yv@gmail.com>
2014-06-19 15:22:39 +0600

    allowed hosts setting

--- food/settings/production.py
+++ food/settings/production.py
@@ -2,6 +2,8 @@ from food.settings.common import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['.wisemeal.ru']
+
 MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', '..', 'food_media'))
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', '..', 'food_static'))
 
