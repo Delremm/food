081f9ab2a3302298a359d7b239c94d16340d1854
delremm <g1m5yv@gmail.com>
2014-07-01 12:03:29 +0600

    order view fixes

--- food_app/views.py
+++ food_app/views.py
@@ -20,7 +20,6 @@ class FlatblocksView(View):
         return HttpResponse(fb.content, content_type='text/plain;charset=utf-8')
 
 
-
 class TextFileView(TemplateView):
     """
     A ``TemplateView`` that sets the proper headers
@@ -30,7 +29,8 @@ class TextFileView(TemplateView):
     def render_to_response(self, context, **response_kwargs):
         response_kwargs['content_type'] = self.content_type
         context['ROOT_URL'] = self.request.build_absolute_uri('/')
-        return super(TextFileView, self).render_to_response(context, **response_kwargs)
+        return super(TextFileView, self).render_to_response(
+            context, **response_kwargs)
 
 
 class OrderView(TemplateView):
@@ -38,20 +38,5 @@ class OrderView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
-        tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-        # context['menus_breakfast'] = Menu.objects.filter(
-        #     date=tomorrow, menu_type=Menu.BREAKFAST)
-        # context['menus_lunch'] = Menu.objects.filter(
-        #     date=tomorrow, menu_type=Menu.LUNCH)
-        # context['menus_dinner'] = Menu.objects.filter(
-        #     date=tomorrow, menu_type=Menu.DINNER)
-        # context['menus_other'] = Menu.objects.filter(
-        #     date=tomorrow, menu_type=Menu.OTHER)
-        context['menus'] = [
-            [
-                Menu.objects.filter(
-                    date=tomorrow,
-                    menu_type=menu_type[0]),
-                menu_type[1]] for menu_type in Menu.MENU_TYPE]
         response = self.render_to_response(context)
         return response
